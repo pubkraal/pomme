4e4b810f96185da0f2e934e0b946f634fe7cc163
John Kraal <jpakraal@gmail.com>
2011-05-27 16:04:23 +0200

    Made URL storage persistant over rehashes / restarts. Also works per channel.

--- modules/urls.py
+++ modules/urls.py
@@ -1,11 +1,21 @@
+import io
+import os
 import re
 import urllib2
 import time
 import traceback
+try:
+    import cPickle as pickle
+except: 
+    import pickle
 
 import lxml.html
 
-URLCACHE = []
+PICKLEFILE = os.path.expanduser("~/.pommeurls")
+try:
+    URLCACHE = pickle.load(io.open(PICKLEFILE, 'r+b'))
+except:
+    URLCACHE = {}
 
 
 def pubmsg(connection, event):
@@ -13,14 +23,19 @@ def pubmsg(connection, event):
     message = event._arguments[0].strip()
     command = message.split()[0].lower()
 
+    uid = '/'.join([connection.server, event.target()])
+    if not uid in URLCACHE:
+        URLCACHE[uid] = []
+
     urlpattern = 'http(s)?://([^\s])+'
     urlmatch = re.search(urlpattern, message)
 
     if urlmatch:
         try:
             if urlmatch.group() not in URLCACHE:
-                URLCACHE = URLCACHE[-4:]
-                URLCACHE.append(urlmatch.group())
+                URLCACHE[uid] = URLCACHE[uid][-4:]
+                URLCACHE[uid].append(urlmatch.group())
+                pickle.dump(URLCACHE, io.open(PICKLEFILE, 'w+b'))
                 title = get_title_from_URL(urlmatch.group())
                 if title:
                     connection.privmsg(event.target(), "title: %s" % (title,))
@@ -28,7 +43,7 @@ def pubmsg(connection, event):
             traceback.print_exc()
 
     if command == '!urls':
-        for url in URLCACHE[:5]:
+        for url in URLCACHE[uid]:
             connection.privmsg(event.target(), url)
             time.sleep(0.5)
 
