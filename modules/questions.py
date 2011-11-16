f922ae31930f32bc32297fff84a1a925a241190c
John Kraal <john@streamtech.nl>
2011-11-16 10:59:55 +0100

    More question stuff

--- modules/questions.py
+++ modules/questions.py
@@ -4,7 +4,12 @@ ANSWERS = ('Google het maar.',
            'Weet ik veel.',
            'M\'n 8-ball zegt "val dood".',
            'Leuk he, dingen vragen aan botjes?',
-           'Wees eerlijk, wat verwacht je nu precies?')
+           'Wees eerlijk, wat verwacht je nu precies?',
+           'De sterren zeggen nee.',
+           '42.',
+           'Zoek het uit',
+           'Does a pope shit in the woods?',
+           'Dat vroeg je vrouw ook vannacht')
 
 NUMBERS = tuple(range(100) + [127, 1337])
 
@@ -15,8 +20,11 @@ def pubmsg(connection, event):
 
     if (message.startswith("pomme, ") or message.startswith("pomme: ")) and \
         message[-1] == '?':
-        if message.split()[1] == 'hoeveel':
+        pieces = message.split()
+        if pieces[1] == 'hoeveel':
             s.privmsg(target, '%s%d' % (random.choice(('ongeveer ', '')),
                                         random.choice(NUMBERS)))
+        elif len(pieces) == 4 and pieces[2] in ('of', 'or'):
+            s.privmsg(target, random.choice((pieces[1], pieces[3][:-1])))
         else:
             s.privmsg(target, random.choice(ANSWERS))
