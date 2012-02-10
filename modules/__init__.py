644791b1afacd1d649f89a5c6ee17f6769f72d6b
John Kraal <john@streamtech.nl>
2012-02-10 11:11:23 +0100

    added some more debugging for rehashing

--- modules/__init__.py
+++ modules/__init__.py
@@ -43,6 +43,7 @@ for modname in sys.modules:
     if modname[:8] != 'modules.' or not modname.split('.')[1] in all:
         continue
     try:
+        print "Reloading", modname
         sys.modules[modname] = reload(sys.modules[modname])
     except:
         traceback.print_exc()
